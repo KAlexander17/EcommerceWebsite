483dbf897ab3e55f0fc27e5cc7c59fc870909f2b
Kyle Alexander <kbalexander17@yahoo.com>
2023-05-19 23:18:33 -0400

    Products updated

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,12 +4,7 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-    { id: 1, name: 'Dragon scale dice', description: 'D&D dice.', price: '$10', image: 'https://cdn11.bigcommerce.com/s-70184/images/stencil/1280x1280/products/3999/11145/IMG_7329__13333.1682447047.jpg?c=2'},
-    { id: 2, name: 'Human Mini', description: 'Human miniature figure', price: '$8', image: 'https://i.etsystatic.com/22523066/r/il/46c180/2769417014/il_570xN.2769417014_j2be.jpg'}
-];
-
-const Products = () => {
+const Products = ({ products }) => {
     const classes = useStyles();
 
     return (
