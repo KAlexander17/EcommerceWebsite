95ced393e745539f98a49ec2d022e26fee165b00
Kyle Alexander <kbalexander17@yahoo.com>
2023-05-26 23:43:51 -0400

    Shipping page finished

--- src/components/CheckoutForm/CustomTextField.jsx
+++ src/components/CheckoutForm/CustomTextField.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { TextField, Grid } from '@material-ui/core';
 import { useFormContext, Controller, useForm } from 'react-hook-form';
 
-const FormInput = ({ name, label, required }) => {
+const FormInput = ({ name, label }) => {
     const { control } = useFormContext();
 
     return (
@@ -12,7 +12,7 @@ const FormInput = ({ name, label, required }) => {
                 fullWidth
                 name={name}
                 render={({field}) => (
-                    <TextField fullWidth label={label} required={required} />
+                    <TextField fullWidth label={label} required />
                 )}
             />
         </Grid>
